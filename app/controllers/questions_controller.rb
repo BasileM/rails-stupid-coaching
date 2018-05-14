40854b4658f9e11d90a7771c7c7b4e64e1766009
Basile Marquefave <basile.marquefave@gmail.com>
2018-05-14 16:53:07 +0200

    exercice 1 - params

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,17 @@
 class QuestionsController < ApplicationController
-  def ask
-  end
+    def ask
 
-  def answer
+    end
+
+    def answer
+      if params[:question] == "I am going to work"
+        @answer = "Great"
+      elsif params[:question].match(/"?"/)
+         @answer2 = "Silly question, get dressed and go to work!"
+      else params[:question]
+        @answer3 = "I don't care, get dressed and go to work!"
+    end
   end
 end
+
+
